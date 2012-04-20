3cf69e6cf147f0965cb1e5098a795b309bbf2d2a
Ron <ronchipi@gmail.com>
2012-04-20 09:24:26 +0300

    Added authentication

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,2 +1,19 @@
 class PeopleController < ApplicationController
+ 
+before_filter :authenticate , :only => :show
+
+ def index
+ 	@people = ["Ron","Oren","Idan"]
+ end
+
+ def show
+ 	@person = params[:id]
+ end
+
+ private
+ def authenticate
+ 	if params[:id] != "Ron"
+ 		redirect_to people_url , :notice => "The page you are trying to access is restricted."
+ 	end
+ end
 end
